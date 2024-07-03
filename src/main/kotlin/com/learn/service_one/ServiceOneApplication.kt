9d0b906eca55a385204f7aff038fe42470499c76
aassalax <aassali.dev@gmail.com>
2024-07-03 19:38:38 +0200

    first commit

---
+++ src/main/kotlin/com/learn/service_one/ServiceOneApplication.kt
@@ -0,0 +1,11 @@
+package com.learn.service_one
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceOneApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceOneApplication>(*args)
+}
